98c0b7e6eb66145a95ab42fcefd3be6406d1b298
Monalisha Mukherjee <121163641+monamukh@users.noreply.github.com>
2022-12-21 16:10:49 +0530

    Updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites ')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale Spinach and Rocket Smoothie')
-streamlit.text('Hard-bolied Free-Range Egg')               
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale Spinach and Rocket Smoothie')
+streamlit.text(' 🐔 Hard-bolied Free-Range Egg') 
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
